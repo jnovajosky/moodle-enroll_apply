f6ec82b8aa8b395e377b7bb357d6c6dde4325ae0
jnovajosky <131738858+jnovajosky@users.noreply.github.com>
2025-10-07 09:51:49 -0700

    Update lib.php

--- lib.php
+++ lib.php
@@ -15,12 +15,9 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * @package    enrol_apply
- * @copyright  emeneo.com (http://emeneo.com/)
+ * @package    moodle-enroll_apply
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- * @author     emeneo.com (http://emeneo.com/)
- * @author     Johannes Burk <johannes.burk@sudile.com>
- * @editor     Josh Novajosky <jnovajosky@gmail.com>
+ * @author     Josh Novajosky <jnovajosky@gmail.com>
  */
 
 /** The user is put onto a waiting list and therefore the enrolment not active (used in user_enrolments->status) */

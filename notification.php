f032b671ad8485a686fc50e937227ed6303857cf
jnovajosky <131738858+jnovajosky@users.noreply.github.com>
2025-10-07 09:52:36 -0700

    Update notification.php

--- notification.php
+++ notification.php
@@ -15,11 +15,9 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * @package    enrol_apply
- * @copyright  2016 sudile GbR (http://www.sudile.com)
+ * @package    moodle-enroll_apply
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- * @author     Johannes Burk <johannes.burk@sudile.com>
- * @editor     Josh Novajosky <jnovajosky@gmail.com>
+ * @author     Josh Novajosky <jnovajosky@gmail.com>
  */
 
 defined('MOODLE_INTERNAL') || die();
